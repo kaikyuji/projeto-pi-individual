934bd946a2b1c24f5a8bae206eb05f9c3f64b833
kaikyuji <kaikykohatsu@gmail.com>
2023-06-04 19:20:59 -0300

    Final DE VDD

--- src/database/script-insercoes.sql
+++ src/database/script-insercoes.sql
@@ -1,14 +1,14 @@
 insert into usuario values
-(null, 'Kaiky', 'kaka', 'kaiykkohatsu@gmail.com', '2004-07-24', null),
+(null, 'Kaiky', 'kaka', 'kaikykohatsu@gmail.com', '2004-07-24', null),
 (null, 'Gustavo', '123', 'gu@hotmail.com', '2004-02-21', null),
 (null, 'Carla', 'batata', 'carla@gmail.com', '2000-12-11', null),
 (null, 'Angela', '123', 'angela@gmail.com', '1999-12-13', null),
 (null, 'Yuji', '123', 'yuji@gmail.com', '2004-07-24', null);
 
 insert into recorde values
-(1, 1, 1000, current_date),
-(1, 2, 3430, current_date),
-(1, 3, 10000, current_date),
-(1, 4, 20, current_date),
-(1, 5, 0, current_date);
+(null, 1, 1000, current_date),
+(null, 2, 3430, current_date),
+(null, 3, 10000, current_date),
+(null, 4, 20, current_date),
+(null, 5, 0, current_date);
 
